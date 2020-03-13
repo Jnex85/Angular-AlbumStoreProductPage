ab5988edb31e2531c6be80c6f1cd163a0d277957
Josef <giuseppe.neutro@outlook.com>
2020-03-13 16:32:07 +0100

    module completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,9 +10,9 @@ import { ProductService } from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
-import {RouterModule, Route } from '@angular/router';
+import {RouterModule, Routes } from '@angular/router';
 
-const appRoutes: Route[] = [
+const appRoutes: Routes = [
   { path:'products', component: ProductListComponent},
   { path:'product/:id', component: ProductPageComponent },
   { path:'', redirectTo: 'products', pathMatch: 'full' },
